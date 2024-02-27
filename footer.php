d42467512e3fb94557747f6a54bcfc6859547dfe
Albert Nabarrete <ahnabarrete@gmail.com>
2024-02-26 23:30:56 -0300

    primeiro commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+</body>
+</html>
+<script rel="stylesheet" href="assets/js/bootstrap.bundle.min.js"></script>
+<script rel="stylesheet" href="assets/js/bootstrap.esm.min.js"></script>
+<script rel="stylesheet" href="assets/js/bootstrap.min.js"></script>
+<script rel="stylesheet" href="assets/js/custom.js"></script>
+<script rel="stylesheet" href="assets/js/holder.min.js"></script>
+<script rel="stylesheet" href="assets/js/jquery-3.2.1.slim.min.js"></script>
+<script rel="stylesheet" href="assets/js/popper.min.js"></script>
+<script rel="stylesheet" defer href="assets/js/app.js"></script>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio|Albert H.Nabarrete</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css.map">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-utilities.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="assets/imagens/logo-ahn.jpeg"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#sobre-mim">Sobre Mim</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#projetos">Projetos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#habilidades">Habilidades</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+<?php 
+include("footer.php")
+?>
